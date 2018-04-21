7c80dbd82aa07bd87512c0b3b8422f3742a47597
Swen Meeuwes <swenmeeu@gmail.com>
2018-04-21 11:44:13 +0200

    Implemented player movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,9 +4,22 @@ public class PlayerMovement : MonoBehaviour
 {
     public float Speed;
 
+    private bool _isMoving;
+
     private void Update()
     {
-        var input = new Vector2(Input.GetAxis(InputAxesLiterals.Horizontal), Input.GetAxis(InputAxesLiterals.Vertical));
-        transform.Translate(input * Speed * Time.deltaTime);
+        _isMoving = InputHandler.Instance.GetAction(InputAxesLiterals.Move);
+        if (_isMoving)
+            MoveHorizontallyToInputPosition();
+    }
+
+    private void MoveHorizontallyToInputPosition()
+    {
+        var screenPosition = InputHandler.Instance.GenericInputPosition;
+        var worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
+        var targetPosition = worldPosition;
+        targetPosition.y = transform.position.y; // Stay on same vertical position
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, Speed * Time.deltaTime);
     }
 }
