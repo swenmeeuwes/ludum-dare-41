8e32670f119b9d5d60760ff745ede084605948b2
Swen Meeuwes <swenmeeu@gmail.com>
2018-04-23 00:11:28 +0200

    Added end of game scene

---
+++ Assets/Scripts/UI/EndOfGameController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndOfGameController : MonoBehaviour
+{
+    [SerializeField] private Animator _textBlinkAnimator;
+
+    private bool _isStarting;
+
+    private void Update()
+    {
+        if (Input.anyKey)
+            StartCoroutine(StartSequence());
+    }
+
+    private IEnumerator StartSequence()
+    {
+        if (_isStarting)
+            yield break;
+
+        SoundManager.Instance.Play(Sound.StartGame);
+
+        _isStarting = true;
+
+        _textBlinkAnimator.speed = 5;
+
+        yield return new WaitForSeconds(0.65f);
+
+        SceneLoader.Instance.LoadSceneAsync(0);
+    }
+}
